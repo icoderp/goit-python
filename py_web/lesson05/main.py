1af66313271c934eaf5c6d976fda2d3885d77b71
icoderp <igor.voyt98@gmail.com>
2022-08-10 13:54:20 +0300

    Added test spead Thread

--- py_web/lesson05/main.py
+++ py_web/lesson05/main.py
@@ -1,4 +1,4 @@
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 from multiprocessing import Pool, cpu_count
 from time import time
 
@@ -65,5 +65,14 @@ if __name__ == '__main__':
         print(f"Test spead <ProcessPoolExecutor> CPU: {n}.")
         start_time = time()
         with ProcessPoolExecutor(max_workers=n) as executor:
+            a, b, c, d = executor.map(factorize, (128, 255, 99999, 10651060))
+            asserting(a[0], b[0], c[0], d[0], start_time)
+
+    print(f"{'_' * 50}\n Test spead Thread")
+
+    for n in range(1, cpu + 1):
+        print(f"Test spead <ThreadPoolExecutor> Thread: {n}.")
+        start_time = time()
+        with ThreadPoolExecutor(max_workers=n) as executor:
             a, b, c, d = executor.map(factorize, (128, 255, 99999, 10651060))
             asserting(a[0], b[0], c[0], d[0], start_time)
